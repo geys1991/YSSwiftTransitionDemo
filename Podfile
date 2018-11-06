ba8b8797eac44bffbef397eaa9bfc85231617a59
yanshengge <yanshengge@yangqianguan.com>
2018-11-06 17:06:30 +0800

    new res

---
+++ Podfile
@@ -0,0 +1,21 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'YSSwiftTransitionDemo' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for YSSwiftTransitionDemo
+
+pod 'Moya', '~> 12.0.0-beta.1'
+  target 'YSSwiftTransitionDemoTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'YSSwiftTransitionDemoUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
---
+++ YSSwiftTransitionDemoTests/YSSwiftTransitionDemoTests.swift
@@ -0,0 +1,34 @@
+//
+//  YSSwiftTransitionDemoTests.swift
+//  YSSwiftTransitionDemoTests
+//
+//  Created by 葛燕生 on 2018/11/6.
+//  Copyright © 2018 葛燕生. All rights reserved.
+//
+
+import XCTest
+@testable import YSSwiftTransitionDemo
+
+class YSSwiftTransitionDemoTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
